601d0a5106217fc1a57ff4fb322e8f69120837de
tomQuintana <tomquintana20@gmail.com>
2023-09-29 12:43:07 -0300

    feat(routes): document created endpoint

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -5,4 +5,27 @@ const route = Router();
 
 route.post('/register', registerBook);
 
+/**
+ * @swagger
+ * /api/books/create:
+ *   post:
+ *     summary: Created a new book 
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: "New Book"
+ *         schema:
+ *          type: object
+ *          proporteis: 
+ *            title:
+ *              type: string
+ *            author:
+ *              type: string
+ *          example:
+ *              title: "Magos de los Dioses"
+ *              author: "Graham Hancock"
+ *              wasRead: by default is false
+ *
+ */
+
 export default route;
