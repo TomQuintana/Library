35850ba1e04663d82bf96c71f3e4aea03a400e4a
tomQuintana <tomquintana20@gmail.com>
2023-09-29 12:40:03 -0300

    feat(docs): config swagger docs

---
+++ src/doc/swagger.ts
@@ -0,0 +1,16 @@
+//TODO: agregar documentacion
+
+export const options = {
+  failOnErrors: true,
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Books',
+      version: '1.0.0',
+      description: 'API Documentation',
+    },
+  },
+  apis: [
+    './src/routes/**/*.ts',
+  ], 
+};
