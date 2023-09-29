5a31ae49edf1d913ffb3cc0ba911f04ef44f726a
tomQuintana <tomquintana20@gmail.com>
2023-09-29 11:38:21 -0300

    feat(service): add service

---
+++ src/services/book.services.ts
@@ -0,0 +1,16 @@
+import { MongoRepository } from "../repository/mongo.repository";
+
+export class BookUseCase {
+
+  mongoRepository = new MongoRepository();
+  
+  public registerBook = async({title, author}: {title: string, author: string}) => {
+    const newBook = {
+      title,
+      author
+    };
+    const bookCreated = await this.mongoRepository.createBook(newBook);
+    return bookCreated;
+  };
+
+}
